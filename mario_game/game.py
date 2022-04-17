0415329e6fa608563889f62cc578aa23d6183732
maxka <maxk4@illinois.edu>
2022-04-17 03:39:16 -0500

    Tried to implement shooter functionality

--- mario_game/game.py
+++ mario_game/game.py
@@ -2,13 +2,14 @@ import pygame as pg
 import constants
 import time
 from enemys.dropper import Dropper
+from enemys.bullet import Bullet
 from enemys.floater import Floater
 from enemys.shooter import Shooter
-from enemys.charger import Charger
-from enemys.jumper import Jumper
-from enemys.flyer import Flyer
-from enemys.mover import Mover
-from mario_game.objects.flag import Flag
+#from enemys.charger import Charger
+#from enemys.jumper import Jumper
+#from enemys.flyer import Flyer
+#from enemys.mover import Mover
+#from mario_game.objects.flag import Flag
 from objects.floor import Floor
 from objects.world_box import WorldBox
 from player import Player
@@ -20,7 +21,8 @@ class Game:
         self.width = constants.SCREEN_WIDTH
         self.height = constants.SCREEN_HEIGHT
         self.player = Player(int(constants.SCREEN_WIDTH / 2), int(constants.SCREEN_HEIGHT / 2))
-        self.enemies = [Dropper(950,0), Floater(800, 20), Charger(1000, 300)]
+        self.enemies = [Dropper(950,0), Floater(800, 20), Shooter(1000, 200)]
+        self.timer = time.time()
         self.objects = [Floor(900,100),Floor(600, 400,width=1000)]
         self.world_box = WorldBox()
         self.running = True
@@ -103,6 +105,10 @@ class Game:
         for enemy in self.enemies:
             enemy.update(keys, self.player, self.enemies)
 
+        #Creates and shoots a bullet enemy every 4 seconds, does not currently work
+        if self.timer % 4 == 0:
+            self.enemies.append(Bullet(1000, 200))
+            Bullet.draw(self, screen)
 
     def handle_inputs(self, keys):
         update_speed = constants.SCREEN_WIDTH / self.fps / 5
