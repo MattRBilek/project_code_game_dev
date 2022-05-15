cef272e2784bc02da5228eabb03bbd1d58c11e61
Matt bilek <mattbilek480@gmail.com>
2022-05-15 12:19:53 -0500

    added outline for json loading

--- mario_game/player.py
+++ mario_game/player.py
@@ -2,25 +2,31 @@ import constants
 import pygame as pg
 import time
 
+image_width = 200
+image_height = 200
+
+
 class Player:
     def __init__(self,x,y):
         self.up = 6
         self.down = 0
         self.x = x
         self.y = y
-        self.width = constants.SCREEN_WIDTH/25 # TODO: make this scale with constancts.SCREEN_WIDTH / some number
-        self.height = constants.SCREEN_HEIGHT/30 # TODO: make this scale with constancts.SCREEN_HEIGHT / some number
+        self.width = 200 #constants.SCREEN_WIDTH/25 # TODO: make this scale with constancts.SCREEN_WIDTH / some number
+        self.height = 200 #constants.SCREEN_HEIGHT/30 # TODO: make this scale with constancts.SCREEN_HEIGHT / some number
         self.color = [200, 100, 100]
         self.grounded = False # should we fall
         self.type = constants.TYPES.PLAYER
         self.y_velocity = 0
         self.left = False
         self.right = False
-        self.walkRight = [pg.image.load('R1.png'), pg.image.load('R2.png'), pg.image.load('R3.png'), pg.image.load('R4.png')]
-        self.walkRight = [pg.image.load('L1.png'), pg.image.load('L2.png'), pg.image.load('L3.png'), pg.image.load('L4.png')]
-
+        self.image = pg.image.load("Spritesheet.png")
+        self.image = pg.transform.scale(self.image, (800, 800))
+        self.moveing_left = True
+        self.image_x = 0
     def draw(self, screen):
-        pg.image.load('R1.png')
+        screen.blit(self.image, (self.x,self.y), ((int(self.image_x) % 4) * image_width,
+                                                  (2 + (not self.moveing_left)) * image_height,image_width,image_height))
 
     def collide(self, world_object):
         pass # TODO: collided with object see type with type
@@ -29,6 +35,13 @@ class Player:
         if keys[pg.K_SPACE] and self.grounded:    # jumping
             self.y_velocity = -20
             self.y += self.y_velocity
+
+        if keys[pg.K_d]:
+            self.moveing_left = False
+            self.image_x += .05
+        elif keys[pg.K_a]:
+            self.moveing_left = True
+            self.image_x += .05
 
         if not self.grounded:
             self.y_velocity += 1
